2e4701369cad96527f1dba5967883e81ccd36de9
hiu <p29hieu@gmail.com>
2020-10-02 22:20:51 +0700

    add layout

--- app/src/main/java/com/example/caculatorbasic/MainActivity.kt
+++ app/src/main/java/com/example/caculatorbasic/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.caculatorbasic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+  }
+
+  override fun onResume() {
+    super.onResume()
+    edit_text_input.inputType = InputType.TYPE_NULL
+    edit_text_input.requestFocus()
   }
 }
