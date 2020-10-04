e4badfcb99f3db0e1e0f4b99fee4a536d8783448
p29hieu <p29hieu@gmail.com>
2020-10-04 21:56:55 +0700

    change style all

--- app/src/main/java/com/example/caculatorbasic/MainActivity.kt
+++ app/src/main/java/com/example/caculatorbasic/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.caculatorbasic
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +14,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
+    override fun onResume() {
+        super.onResume()
+        edit_text_input.inputType = InputType.TYPE_NULL
+    }
 
 }
